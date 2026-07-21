b431935d4b9537fcc2d7dd235a5ee8e7f0c20871
Vasshell <i-will-set-it-up-@later.com>
2026-07-21 13:58:49 +0300

    minor tweaks

--- src/main/java/vasshell/dservice/rabbitmq/RabbitMessagingService.java
+++ src/main/java/vasshell/dservice/rabbitmq/RabbitMessagingService.java
@@ -1,10 +1,12 @@
 package vasshell.dservice.rabbitmq;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
-import vasshell.dservice.config.RabbitConfig;
 import vasshell.dservice.user.UserMessagingService;
+import vasshell.dservice.user.UserService;
 import vasshell.dservice.user.dto.CreateUserDto;
 import vasshell.dservice.user.dto.UpdateUserDto;
 
@@ -15,10 +17,12 @@ public class RabbitMessagingService implements UserMessagingService {
 
     private final ObjectMapper objectMapper;
     private final RabbitTemplate rabbitTemplate;
+    private final UserService userService;
 
-    public RabbitMessagingService(ObjectMapper objectMapper, RabbitTemplate rabbitTemplate) {
+    public RabbitMessagingService(ObjectMapper objectMapper, RabbitTemplate rabbitTemplate, UserService userService) {
         this.objectMapper = objectMapper;
         this.rabbitTemplate = rabbitTemplate;
+        this.userService = userService;
     }
 
     @Override
@@ -34,5 +38,21 @@ public class RabbitMessagingService implements UserMessagingService {
     @Override
     public void updateUserMessage(UpdateUserDto user) {
         rabbitTemplate.convertAndSend(RabbitConfig.QUEUE_UPDATE, objectMapper.writeValueAsBytes(user));
+    }
+
+    @RabbitListener(queues = RabbitConfig.QUEUE_CREATE)
+    public void receiveCreateMessage(Message message){
+        userService.createUser(objectMapper.readValue(message.getBody(), CreateUserDto.class));
+    }
+
+    @RabbitListener(queues = RabbitConfig.QUEUE_DELETE)
+    public void receiveDeleteMessage(Message message){
+        userService.deleteUser(objectMapper.readValue(message.getBody(), UUID.class));
+    }
+
+    @RabbitListener(queues = RabbitConfig.QUEUE_UPDATE)
+    public void receivePatchMessage(Message message){
+        UpdateUserDto request = objectMapper.readValue(message.getBody(), UpdateUserDto.class);
+        userService.updateUser(request);
     }
 }
