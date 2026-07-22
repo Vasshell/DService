6e0d75d5f5da296f52abb641241eb53ccb0aa550
Vasshell <i-will-set-it-up-@later.com>
2026-07-22 13:08:45 +0300

    Specification logic moved to service

--- src/main/java/vasshell/dservice/Application.java
+++ src/main/java/vasshell/dservice/Application.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
-public class DServiceApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(DServiceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/vasshell/dservice/service/UserService.java
+++ src/main/java/vasshell/dservice/service/UserService.java
@@ -1,6 +1,8 @@
 package vasshell.dservice.service;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.PredicateSpecification;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
@@ -31,7 +33,7 @@ public class UserService {
 
     public List<UserGetDto> findAll(UserFilterParamsDto params, Pageable pageable){
         return mapper.userToGetDto(
-                userRepo.findAll(params.toSpecification(), pageable)
+                userRepo.findAll(toSpecification(params), pageable)
                         .getContent());
     }
 
@@ -40,7 +42,7 @@ public class UserService {
     }
 
     @Async
-    public void createUser(UserCreateDto user){
+    public void createUser(UserDto user){
         createUser(mapper.createDtoToUser(user));
     }
 
@@ -61,5 +63,57 @@ public class UserService {
         if (userUpdateDto.age() != null) userToUpdate.setAge(userUpdateDto.age());
 
         userRepo.save(userToUpdate);
+    }
+
+    private Specification<User> toSpecification(UserFilterParamsDto params){
+        Specification<User> spec = Specification.unrestricted();
+
+        if (params.firstName() != null){
+            spec = spec.and(UserSpecification.likeFirstName(params.firstName()));
+        }
+        if (params.lastName() != null){
+            spec = spec.and(UserSpecification.likeLastName(params.lastName()));
+        }
+        if (params.age() != null){
+            spec = spec.and(UserSpecification.hasAge(params.age()));
+        }
+        if (params.ageGreaterThan()!=null){
+            spec = spec.and(UserSpecification.hasAgeGreaterThan(params.ageGreaterThan()));
+        }
+        if (params.ageLessThan()!=null){
+            spec = spec.and(UserSpecification.hasAgeLessThan(params.ageLessThan()));
+        }
+        return spec;
+    }
+}
+
+class UserSpecification {
+
+    public static PredicateSpecification<User> likeFirstName(String firstName){
+        return funnyThing(firstName, "firstName");
+    }
+
+    public static PredicateSpecification<User> likeLastName(String lastName){
+        return funnyThing(lastName, "lastName");
+    }
+
+    private static PredicateSpecification<User> funnyThing(String compareTo, String propertyName){
+        return (root, criteriaBuilder) ->
+                criteriaBuilder.like(criteriaBuilder.lower(root.get(propertyName)), "%" + compareTo.toLowerCase() + "%");
+    }
+
+    public static PredicateSpecification<User> hasAge(Integer age) {
+        return (from, criteriaBuilder) ->
+                criteriaBuilder.equal(from.get("age"), age);
+    }
+
+    public static PredicateSpecification<User> hasAgeGreaterThan(Integer ageGt) {
+        return (from, criteriaBuilder) ->
+                criteriaBuilder.greaterThan(from.get("age"), ageGt);
+    }
+
+    public static PredicateSpecification<User> hasAgeLessThan(Integer ageLt) {
+        return (from, criteriaBuilder) ->
+                criteriaBuilder.lessThan(from.get("age"), ageLt);
     }
 }
